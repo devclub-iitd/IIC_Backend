9065e86064f502c0aef74269021f612dbe811961
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2019-12-02 00:21:06 +0530

    added starter code

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('Express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send("You've reached the default start page");
+});
+
+app.get('/hello', function(req, res) {
+	res.send("hey!!");
+});
+
+app.listen(3000, process.env.IP, function() {
+	console.log("Server started");
+});
