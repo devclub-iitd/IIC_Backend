f891a201b5f501f72bd8a9c4a52f1ce79a34db5d
Atishya Jain <atishya.jain@rocketmail.com>
2019-12-05 20:46:10 +0530

    Added cors and other necessary flags

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-var express = require('express');
-var app = express();
+const helmet = require("helmet");
+const compression = require("compression");  // compresses requests
+const bodyParser = require("body-parser");
+const lusca = require("lusca");
+const cors = require("cors");
+// import path from "path";
+const express = require('express');
+const app = express();
+
+app.use(compression());
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
 
 app.get('/', function(req, res) {
   res.send("You've reached the default start page");
